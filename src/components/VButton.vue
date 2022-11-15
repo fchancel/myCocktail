c1ff01c8516d81f7aca9f9a651c4e392b6910a1c
Florian CHANCELADE <florian.chancelade@gmail.com>
2022-11-15 09:57:33 +0100

    Adding meta balise and config tailwind for hover

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -4,9 +4,9 @@ defineProps<{ title: string; disabled?: boolean }>();
 
 <template>
   <button
-    class="p-3 w-full text-sm rounded-xl border-2 border-black dark:border-pink-400 hover:scale-105 duration-300"
+    class="p-3 w-full text-sm rounded-xl border-2 border-black bg-neutral-100 dark:bg-neutral-800/30 dark:border-pink-400 hover:scale-105 duration-300"
     :class="{
-      'border-black/30 text-black/30 dark:text-yellow-400/20 dark:border-pink-400/20 hover:scale-100':
+      'border-black/30 text-black/30 dark:text-white/30 dark:border-pink-400/20 hover:scale-100':
         disabled,
     }"
     :disabled="disabled"
